c46ab87c2deeb31e8bceeec6baff16c7c23a4944
EdsonMello-code <edsonmelo754@gmail.com>
2021-07-18 05:59:02 -0300

    feat: Added routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,26 @@
-import { Router } from "express";
+import { Router } from 'express';
+import ProductController from './controllers/ProductController';
+import StoreController from './controllers/StoreController';
+import auth from './middlewares/auth';
 
 const routes = Router();
+
+const storeController = new StoreController() 
+const productController = new ProductController() 
+
+routes.get('/users/products', productController.index);
+routes.get('/users/stores', productController.index);
+
+
+routes.post('/admin/stores/register', storeController.create);
+routes.post('/admin/stores/authenticate', storeController.authenticate);
+
+routes.post('/admin/stores/products', productController.create);
+routes.get('/admin/stores/products', productController.index);
+
+routes.use(auth)  
+routes.get('/admin/stores', storeController.index);
+routes.get('/admin/store/products', productController.index);
+
 
 export default routes;
