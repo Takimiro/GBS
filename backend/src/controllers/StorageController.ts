82815920f4ad4093b116ab91c8c487c8ef896b63
EdsonMello-code <edsonmelo754@gmail.com>
2021-07-20 08:16:13 -0300

    fix: Removed import unused and rename vars

--- backend/src/controllers/StorageController.ts
+++ backend/src/controllers/StorageController.ts
@@ -1,17 +1,17 @@
-import { Request, response, Response } from 'express';
+import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import  { Storage }   from '../models/Storage';
 
 export default class StorageController {
   async create(request: Request, response: Response) {
-    let storeRequested = request.body as Storage;
+    let storageRequested = request.body as Storage;
     const { id } = request.params;
 
     try {
       const repository = getRepository(Storage);
-      const storeCreated = repository.create({ userId: parseInt(id),...storeRequested });
-      const storeSaved = await repository.save(storeCreated);
-      return response.json(storeSaved);
+      const storageCreated = repository.create({ userId: parseInt(id),...storageRequested });
+      const storageSaved = await repository.save(storageCreated);
+      return response.json(storageSaved);
     } catch (error) {
       return response.status(400).json(error)
     }
@@ -20,35 +20,12 @@ export default class StorageController {
   async index(_request: Request, response: Response) {
     try {
       const repository = getRepository(Storage);
-      const store = await repository.find({ relations: ['items'] });
-      console.log(store)
-      return response.json(store);
+      const storage = await repository.find({ relations: ['items'] });
+      console.log(storage)
+      return response.json(storage);
     } catch (error) {
       console.log(error)
       return response.status(400).json(error)
     }
   }
-
-  // async update(request: Request, response: Response) {
-  //   const { id } = request.params as Storage;
-  //   const requestedStore = request.body as Storage;
-  //   try {
-  //     const repository = getRepository(Storage);
-  //     await repository.update(id!, requestedStore);
-  //     return response.send();
-  //   } catch (error) {
-  //     return response.status(400).json(error)
-  //   }
-  // }
-
-  // async delete(request: Request, response: Response) {
-  //   const { id } = request.params as Storage;
-  //   try {
-  //     const repository = getRepository(Storage);
-  //     await repository.delete(id!);
-  //     return response.send();
-  //   } catch (error) {
-  //     return response.status(400).json(error)
-  //   }
-  // }
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,7 +3,6 @@ import ItemController from './controllers/ItemController';
 import StorageController from './controllers/StorageController';
 import UserController from './controllers/UserController';
 
-
 const routes = Router();
 
 const userController = new UserController() 
@@ -15,20 +14,15 @@ routes
   .post('/users', userController.create)
   .get('/users/:id', userController.show);
 
-
 // storages 
 routes
   .post('/users/:id/storages', storageController.create)
   .get('/users/:id/storages', userController.show);
 
-
-
 // items
 routes
   .post('/users/:userId/storages/:id/items', itemController.create)
-  .get('/users/:userId/storages/:id/items', itemController.index)
+  .get('/users/:userId/storages/items', itemController.index)
   .get('/users/storages/items', itemController.search);
-
-
 
 export default routes;
